d4d4d0f834be15a59e9f7cb0126fc979103fa378
Dipesh Patel <mr.dvpatel@gmail.com>
2013-07-20 13:34:08 -0400

    comment update.

--- src/main/java/patel/dipesh/jsr303/model/Person.java
+++ src/main/java/patel/dipesh/jsr303/model/Person.java
@@ -18,76 +18,100 @@ public class Person implements Serializable{
 	private Integer age ;
 	private String phone ;
 	private Address address ;
-
 	
 	/*
-	 * Accessors for setting Person's first name.
+	 * Setter for Person's first name.
+	 * @param inp first name.
 	 */
 	public void setFirstName(String inp) {		
 		this.firstName = inp ;		
 	}
 	
+	/*
+	 * Getter for Person's first name.
+	 * @return person's first name
+	 */	
 	public String getFirstName() {
 		return this.firstName ;		
 	}
-	
-	
+		
 	/*
-	 * Accessors for setting Person's last name.
+	 * Setter for Person's last name.
+	 * @param inp last name.
 	 */
 	public void setLastName(String inp) {		
 		this.firstName = inp ;		
 	}
 	
+	/*
+	 * Getter for Person's last name.
+	 * @return person's last name
+	 */	
 	public String getLastName() {
 		return this.lastName ;		
 	}
 	
-
 	/*
-	 * Accessors for setting Person's email address.
+	 * Setter for Person's email address.
+	 * @param inp email address. 
 	 */
 	public void setEmail(String inp) {		
 		this.eMail = inp ;		
 	}
 	
+	/*
+	 * Getter for Person's email address.
+	 * @return person's email address
+	 */		
 	public String getEmail() {
 		return this.eMail ;		
 	}
 
-
 	/*
-	 * Accessors for setting Person's age.
+	 * Setter for Person's age.
+	 * @param inp age. 
 	 */
 	public void setAge(Integer inp) {		
 		this.age = inp ;		
 	}
-	
+
+	/*
+	 * Getter for Person's age.
+	 * @return person's age 
+	 */	
 	public Integer getAge() {
 		return this.age ;		
 	}
 
 	/*
-	 * Accessors for setting Person's phone number.
+	 * Setter for Person's phone number, US only.
+	 * @param inp phone number (US only). 
 	 */
 	public void setPhone(String inp) {		
 		this.phone = inp ;		
 	}
 	
+	/*
+	 * Getter for Person's phone number, US format.
+	 * @return person's phone number
+	 */		
 	public String getPhone() {
 		return this.phone ;		
 	}
 
-	
 	/*
-	 * Accessors for setting Person's Address.
+	 * Setter for Person's Address.
+	 * @param inp Address object. 
 	 */
 	public void setAddress(Address inp) {		
 		this.address = inp ;		
 	}
 	
+	/*
+	 * Getter for Person's Address.
+	 * @return person's Address
+	 */		
 	public Address getAddress() {
 		return this.address ;		
 	}
-
 }
